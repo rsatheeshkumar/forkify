e56add79bc9708292ab0c3fff38aa6fadfd1ca8f
satheesh <rsatheeshkumar2894@gmail.com>
2020-12-16 10:27:14 +0530

    Project uploaded

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,20 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
+import { key, proxy } from '../config';
 
 export default class Search {
-  constructor(query) {
-    this.query = query;
-  }
-
-  async getResults() {
-    try {
-      const res = await axios(
-        `https://forkify-api.herokuapp.com/api/search?q=${this.query}`
-      );
-      // console.log(res);
-      this.result = res.data.recipes;
-      // console.log(this.result);
-    } catch (error) {
-      alert(error);
+    constructor(query) {
+        this.query = query;
     }
-  }
+
+    async getResults() {
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,30 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ["babel-polyfill", "./src/js/index.js"],
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/bundle.js",
-  },
-  devServer: {
-    contentBase: "./dist",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: "pre",
-        use: ["source-map-loader"],
-      },
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
     ],
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.js$/,
-        enforce: "pre",
-        use: ["source-map-loader"],
-      },
-    ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
